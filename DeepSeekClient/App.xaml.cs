4e00b863ab3487db16812b1994e6d3036b2be8a5
yangetha <yangshiyao@ybl-ist.com>
2025-03-08 11:26:25 +0800

    添加项目文件。
    更新项目结构，集成 Prism 框架
    
    - 在 `.gitignore` 中添加自定义设置。
    - 修改 `App.xaml`，将根类更改为 `PrismApplication`，并更新命名空间。
    - 在 `App.xaml.cs` 中添加 Prism 相关命名空间，新增 `CreateShell` 和 `RegisterTypes` 方法。
    - 更新 `MainWindow.xaml` 和 `MainWindow.xaml.cs` 的命名空间，以符合新的项目结构。

---
+++ DeepSeekClient/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using DeepSeekClient.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DeepSeekClient;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : PrismApplication
+{
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+}
